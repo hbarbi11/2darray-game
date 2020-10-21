f22d9041b8073c2a062dadab945fb1cdb29fa3ab
hbarbi11 <hegedusbarby@gmail.com>
2020-10-21 15:31:41 +0200

    map

---
+++ map.js
@@ -0,0 +1,11 @@
+const fillMap = (width, height, apples, player) => {
+  const array = new Array(width);
+  for (let i = 0; i < width; i++) {
+    array[i] = new Array(height);
+  }
+  return array;
+};
+
+module.exports = {
+  fillMap
+};
